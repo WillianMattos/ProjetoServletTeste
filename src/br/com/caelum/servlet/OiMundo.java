d10d4167b0d3ee2e3f6069cfa75ebe65baa4e0e4
WillianMattos <will07_mattos@hotmail.com>
2016-12-01 17:32:35 -0200

    Projeto Inicial de um servlet Basico
    Apenas o Inicio de um projeto servlet para testar meus conhecimentos.

---
+++ src/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,32 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OiMundo extends HttpServlet {
+	protected void service (HttpServletRequest request,
+	HttpServletResponse response)
+		throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		/*---------------------------HTML------------------------*/
+		out.println("<html>");
+			out.println("<head>");
+				out.println("<title>Home</title>");
+			out.println("</head>");
+			out.println("<body>");
+				out.println("<h1>Envie-me um e-mail:</h1>");
+				out.println("<form>");
+					out.println("<input type='text' name='email'>");
+					out.println("<input type='reset'>");
+				out.println("</form>");
+			out.println("</body>");
+		out.println("</html>");
+		/*-------------------------------------------------------*/
+	}
+}
